3b4832769d7698d8664caa0125e57d1676215f2c
VIPIN KUMAR BHASKER <87085820+vipin2069@users.noreply.github.com>
2023-09-28 01:22:20 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from config import API_KEY
 
-# Replace with your MongoDB connection details
+# MongoDB connection details
 MONGO_URI = "mongodb://localhost:27017"
 client = MongoClient(MONGO_URI)
 
